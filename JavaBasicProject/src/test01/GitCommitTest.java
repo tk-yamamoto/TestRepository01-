487ce2d1ea183663686a75776110fdeb137dc0bf
tk-yamamoto <takashi.-@nifty.com>
2015-10-04 13:39:49 +0900

    コミットテスト

---
+++ JavaBasicProject/src/test01/GitCommitTest.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright 2015 EIS Co., Ltd. All rights reserved.
+ */
+package test01;
+
+/**
+ * @author （作成者） <br />
+ *  （クラス論理名） <br />
+ *  （説明） <br />
+ *  更新履歴 yyyy/mm/dd （更新者）：（説明） <br />
+ */
+public class GitCommitTest {
+
+  /**
+   *
+   */
+  public GitCommitTest() {
+    // TODO 自動生成されたコンストラクター・スタブ
+  }
+
+  /**
+   * （メソッド論理名） <br />
+   * （説明） <br />
+   * @param args
+   */
+  public static void main(String[] args) {
+    // TODO 自動生成されたメソッド・スタブ
+    System.out.println( "コミットテストです。" );
+    System.out.println( "改修しました。" );
+  }
+
+}
